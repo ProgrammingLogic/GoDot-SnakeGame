c90c4b2ac87131ac9069056e7ddd06c8f6903630
Jonathyn Stiverson <43549238+ProgrammingLogic@users.noreply.github.com>
2024-02-03 17:37:03 -0600

    Implement snake movement

--- src/Snake.gd
+++ src/Snake.gd
@@ -1,36 +1,33 @@
 extends Area2D
 
-
-var speed = Vector2(50, 50)
-
-# Direction defaults to right
-var direction = Vector2(1, 0) 
+var speed = 1
+var velocity = Vector2(0, 0)
+var screen_size
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	position = get_viewport_rect().get_center()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
 	# Set direction to right
-	if Input.is_action_pressed("turn_right"): 
-		direction.x = 1
-		direction.y = 0
+	if Input.is_action_pressed("turn_right"):
+		velocity.x = speed * 1
+		velocity.y = 0
 	# Set direction to left
 	elif Input.is_action_pressed("turn_left"): 
-		direction.x = -1
-		direction.y = 0
+		velocity.x = speed * -1
+		velocity.y = 0
 	# Set direction to up
 	elif Input.is_action_pressed("turn_up"):
-		direction.x = 0
-		direction.y = -1
+		velocity.x = 0
+		velocity.y = speed * -1
 	# Set direction to down
 	elif Input.is_action_pressed("turn_down"):
-		direction.x = 0
-		direction.y = 1
-		
+		velocity.x = 0
+		velocity.y = speed * 1		
 
 	# Move the snake
-	position += (direction * speed) * delta
+	position += velocity
