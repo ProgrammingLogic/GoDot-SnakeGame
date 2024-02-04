74bdbd03d6f5117b12f2f6d360351b9ecf9d33b1
Jonathyn Stiverson <43549238+ProgrammingLogic@users.noreply.github.com>
2024-02-03 19:00:13 -0600

    Update snake so it doesn't go outside the screen

--- src/Snake.gd
+++ src/Snake.gd
@@ -1,33 +1,66 @@
 extends Area2D
 
-var speed = 1
+var speed = 200
 var velocity = Vector2(0, 0)
+var screen
 var screen_size
+var center_of_screen
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = get_viewport_rect().get_center()
+	screen = get_viewport_rect()
+	screen_size = screen.get_area()
+	center_of_screen = screen.get_center()
+	
+	position = center_of_screen
+	
+	print(rotation)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):	
-	# Set direction to right
+func _process(delta):
+	var new_rotation
+		
 	if Input.is_action_pressed("turn_right"):
+		new_rotation = 2 * PI
 		velocity.x = speed * 1
 		velocity.y = 0
-	# Set direction to left
 	elif Input.is_action_pressed("turn_left"): 
+		new_rotation = PI
 		velocity.x = speed * -1
 		velocity.y = 0
-	# Set direction to up
 	elif Input.is_action_pressed("turn_up"):
+		new_rotation = PI / 2
 		velocity.x = 0
 		velocity.y = speed * -1
-	# Set direction to down
 	elif Input.is_action_pressed("turn_down"):
+		new_rotation = (3 * PI) / 2
 		velocity.x = 0
 		velocity.y = speed * 1		
 
+
 	# Move the snake
-	position += velocity
+	position += velocity * delta
+	
+	if rotation != TYPE_NIL:
+		rotation = new_rotation
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	# If we are to the right of the screen
+	if position.x > screen.size.x:
+		position.x = 0
+	# If we are to the left of the screen
+	elif position.x < 0:
+		position.x = screen.size.x
+	
+	# If we are below the screen 
+	if position.y > screen.size.y:
+		position.y = 0
+	# If we are above the screen
+	elif position.y < 0:
+		position.y = screen.size.y
+	
+	
+	
